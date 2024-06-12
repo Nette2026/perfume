1455f392192241401fd3778e6366236761540cb0
Nette2026 <twon2026@gmail.com>
2024-06-12 14:29:32 -0500

    working on routes in node

---
+++ node/app/config/dbconfig.js
@@ -0,0 +1,13 @@
+// This connects to my database
+
+const mysql = require('mysql2')
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'perfume_db'
+})
+
+module.exports = pool
